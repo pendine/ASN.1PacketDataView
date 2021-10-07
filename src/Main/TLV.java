08409cdc9c228e0f511e62168322b6d5c3559790
PARK SE JIN <s7734@it-cous.com>
2021-10-07 11:53:06 +0900

    [기능추가] TLV 클래스 기능추가
    입력된 바이트 배열을 삽입했을때
    Tag, Length 확인 후 해당 바이트 저장

--- src/Main/TLV.java
+++ src/Main/TLV.java
@@ -4,104 +4,94 @@ import util.ByteToHex;
 
 public class TLV {
 	
-	public String Tag;
-	public String Length;
-	public String[] Value;
-//	public TLV[] innerTLV;
+	public byte tag;
+	public byte length;
+	public int lengthValue;
+	public byte[] Value;
 	
 	public int grade;
-	public int startPoint;
-	public int endPoint;
 	
-	public String[] motherStr;
+	public byte[] receivedBytes;
 	
-	public TLV( String[] motherStr, int grade, int startPoint ) {
-		Tag = motherStr[startPoint];
-		this.grade = grade;
-		startPoint += 1;
-		Length = motherStr[ ( startPoint ) ];
-		startPoint += 1;
+	private int index = 0;
+	
+//	필수 요소값은 Tag, Length 두개.
+//	
+//	Length가 0이라면 Value는 null;
 
-		int checkLengthResult = checkLength( Length );
+//	구현방법.
+//	전달받은 바이트배열의 길이가 2보다 작다면 그냥 Value에 삽입하거나 에러 등 따로 처리할것.
+//	2보다 크다면 
+//	첫번째 위치의 바이트를 Tag에 삽입.
+//	두번째 위치의 바이트를 Length에 삽입.
+//	
+//	Length가 0x7f 이하라면 그대로 int형으로 변환한뒤,
+//	Tag의 값이 구조체를 표현하는 태그값이 아닌 경우에
+//	나머지의 값을 Value에 삽입.
+//	
+//	Length가 0x80 이상이라면 0x80 으로 빼준 뒤 나온값만큼 추가로 읽고,
+//	추가로 읽은 만큼 바이트를 int혹은 long으로 변환,
+//	Tag의 값이 구조체를 표현하는 태그값이 아닌 경우에
+//	나머지의 값을 Value에 삽입.
+//	
+//	만약에 태그값이 구조체라면 새로운 구조체를 생성한다.
+	
+	public TLV( byte[] receivedBytes, int grade ) {
+		this.grade = grade;
 		
-		int length = 0;
-		
-		if(checkLengthResult > 0) {
-			StringBuilder tmpSb = new StringBuilder();
-			StringBuilder lengStrSb = new StringBuilder();
-			lengStrSb.append(Length);
-			for(int i=0; i<checkLengthResult; i++) 
-			{
-				tmpSb.append( motherStr[startPoint] );
-				lengStrSb.append( motherStr[startPoint] );
-				startPoint += 1;
-			}
-			
-			length = ByteToHex.hexToInt( tmpSb.toString() );
-		}
-		else if (checkLengthResult == 0) {
-			length = ByteToHex.hexToInt(Length);
-		}
-		else if (checkLengthResult == -1 ) {
-			length = ByteToHex.hexToInt(Length);
-		}
-		
-		System.out.println( "TLV 확인중 L값 : " + length );
-		if( (startPoint + length) > motherStr.length ) {
-			System.out.println(" V의 시작위치 : " + startPoint 
-					+ " 부터 길이값 : " + length 
-					+ "를 합하니 배열의 길이" + motherStr.length 
-					+ " 를 초과 (" + (startPoint + length)
-					+  ")해서 중지함.");
+		if(receivedBytes.length < 2) {
+			this.Value = receivedBytes;
 			return;
 		}
 		
-		Value = new String[length];
+		setTag( receivedBytes , index );
+		setLength( receivedBytes , index );
 		
-		int valIdx = startPoint;
-		for(int i = 0 ; i  < length; i++) {
-			Value [ i ] = motherStr[ i + valIdx  ];
-		}
-		
-//		InputString.grandmom.append(this.getString());
-//		
-//		if(Value.length > 3) {
-////			System.out.println("길이 3 초과. 배열에서 시작 위치 : " + startPoint + " 종료되는 위치 : " + (getLengthInt() + startPoint ));
-////			TLV tmp = new TLV(motherStr, grade+1, startPoint);
-////			ValueDecode.decoding(motherStr, grade+1, startPoint, tmp.getLengthInt() + startPoint );
-//			ValueDecode.decoding(motherStr, grade+1, startPoint, getLengthInt() + startPoint );
-//		}
+		lengthValue = checkLength(length);
 		
 	}
 	
-//	public TLV( String[] motherStr, int grade, int startPoint , int length) {
-//		if(length + startPoint >= motherStr.length ) {
-//			new TLV(motherStr, grade, startPoint);
-//		}
-//		else { 
-//			new TLV(motherStr, grade, startPoint);
-//		}
-//	}
+	//Tag 입력, 내부인덱스 증가.
+	private void setTag( byte[] received , int index ) {
+		this.tag = received[index];
+		increaseIndex();
+	}
+	
+	//Length 입력, 내부인덱스 증가.
+	private void setLength( byte[] received , int index ) {
+		this.length = received[index];
+		increaseIndex();
+		
+		// length 바이트의 크기가 1보다 클경우
+		// longForm에 맞게 변경해줘야함.
+		int isLongForm = checkLength(this.length);
+		if ( isLongForm > 2 ) 
+		{
+			byte[] lengthValue = new byte[isLongForm];
+			increaseIndex();
+			for( int i = 0; i < isLongForm;  i++) {
+				lengthValue[i] = received[index];
+				increaseIndex();
+			}
+		}
+			
+	}
+	
+	private void increaseIndex() {
+		this.index += 1;
+	}
+	
+	@SuppressWarnings("unused")
+	private void increaseIndex(int value) {
+		this.index += value;
+	}
 	
 	public int getLengthInt() {
-		return ByteToHex.hexToInt( this.Length );
+		return lengthValue;
 	}
 
-	public String getLengthStr() {
-		return this.Length;
-	}
-
-	public int getPacketLength() {
-		int val = 0;
-		if(getTag() == null ) return 0;
-		val += ( getTag().length() / 2 );
-		val += ( getLengthStr().length() / 2 );
-		val += ( Value.length );
-		return val;
-	}
-
-	public String getTag() {
-		return this.Tag;
+	public byte getTag() {
+		return this.tag;
 	}
 	
 	public String getString() {
@@ -109,9 +99,9 @@ public class TLV {
 		StringBuilder sb = new StringBuilder();
 		
 		sb.append( setGradeStr() );
-		sb.append( Tag );
+		sb.append( tag );
 		sb.append( " " );
-		sb.append( Length );
+		sb.append( length );
 		sb.append( "\n" );
 		sb.append( setGradeStr() );
 		sb.append( "\t" );
@@ -146,28 +136,17 @@ public class TLV {
 		return sb.toString();
 	}
 	
-	public int checkLength(String Length) {
-		if ( ByteToHex.hexToInt(Length) == 0x80 ) {
-			return -1;
+	public int checkLength(byte length) {
+		if ( length <= 0x80 ) {
+			return 1;
 		}
 		
-		if ( ByteToHex.hexToInt(Length) > 0x80 ) {
-			return ByteToHex.hexToInt(Length) - 0x80;
+		if ( length > 0x80 ) {
+			return length - 0x80;
 		}
 		
 		return 0;
 	}
 	
-	//길이 값에 맞는지 확인할 용도였는데
-	//127이 넘어가는 값이 있다면 문제 발생.
-	//먼저 주석처리.
-//	public boolean isLength(String Length) {
-//		int tmp = ByteToHex.hexToInt(Length);
-//		if ( tmp < 0x90) {
-//			return ByteToHex.hexToInt(Length) - 0x80;
-//		}
-//		
-//		return 0;
-//	}
 
 }
